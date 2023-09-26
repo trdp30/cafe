6e08d2fbc10f0218e2b62d18ab162d111cc19155
Tujit Bora <tujitbora4@gmail.com>
2023-09-26 23:41:42 +0530

    Update menuItems.js

--- src/menuItems.js
+++ src/menuItems.js
@@ -5,53 +5,55 @@ export const menuItems = [
       { name: "Veg Sandwich", price: 30 },
       { name: "Veg Cheese Sandwich", price: 40 },
       { name: "French Fries", price: 20 },
-      // { name: "Veg Burger", price: 40 },
-      // { name: "Veg Cheese Burger", price: 50 },
+      { name: "Masala French Fries", price: 25 },
+      { name: "Peri Peri French Fries", price: 30 },
+      { name: "Veg Burger", price: 50 },
+      // { name: "Veg Cheese Burger", price: 60 },
       // { name: "Veg Biryani", price: 80 }
     ]
   },
   {
     category: "Juices",
     items: [
-      { name: "Lemon Juice", price: 15 },
-      // { name: "Lemon Soda", price: 20 },
+      { name: "Lemon Juice", price: 20 },
+      // { name: "Lemon Soda", price: 30 },
       {
-        name: "Mix Fruit Juice (Banana, Apple, Milk, Honey, Grapes, Almonds, Dates, Pomegranate/Watermelon)",
+        name: "Mix Fruit Juice (Banana, Apple, Milk, Honey, Grapes, Almonds, Dates, Watermelon)",
         price: 50
       },
-      { name: "Mosambi Juice", price: 35 },
-      { name: "Grape Juice", price: 35 },
-      { name: "Apple Juice", price: 40 },
-      { name: "Pineapple Juice", price: 35 },
-      // { name: "Watermelon Juice", price: 35 }
+      { name: "Mosambi Juice", price: 40 },
+      { name: "Grape Juice", price: 40 },
+      { name: "Apple Juice", price: 50 },
+      { name: "Pineapple Juice", price: 40 },
+      { name: "Watermelon Juice", price: 50 }
     ]
   },
   {
     category: "Hot Drink",
     items: [
       { name: "Black Tea", price: 10 },
-      { name: "Plain Tea", price: 10 },
+      { name: "Milk Tea", price: 10 },
       { name: "Ginger Tea", price: 10 },
       { name: "Cardamom/Elaichi Tea", price: 10 },
       { name: "Lemon Honey Warm Water", price: 15 },
-      { name: "Cappuccino", price: 15 },
-      { name: "Black Coffee", price: 15 }
+      { name: "Cappuccino", price: 20 },
+      { name: "Black Coffee", price: 10 }
     ]
   },
   {
     category: "Milkshake",
     items: [
       { name: "Banana Milkshake", price: 40 },
-      { name: "Apple Milkshake", price: 45 },
-      { name: "Oreo Chocolate Milkshake", price: 45 },
-      { name: "Dry Fruit Milkshake", price: 60 }
+      { name: "Apple Milkshake", price: 50 },
+      { name: "Oreo Chocolate Milkshake", price: 60 },
+      { name: "Dry Fruit Milkshake", price: 70 }
     ]
   },
   {
     category: "More……..",
     items: [
       // { name: "Cold Coffee", price: 50 },
-      { name: "Fruit Bowl (A Bowl of Sliced 5 Different Fruits)", price: 40 }
+      { name: "Fruit Bowl (A bowl of sliced 5 different fruits)", price: 40 }
     ]
   },
   {
