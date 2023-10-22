3c82d12ceca176a67059a281cf075be9c1cd3d35
tujit <tujit.bora@widas.in>
2023-10-22 08:47:00 +0530

    fix(): menu update

--- src/menuItems.js
+++ src/menuItems.js
@@ -1,31 +1,120 @@
 export const menuItems = [
   {
-    category: "Food",
+    category: "Rolls",
     items: [
-      { name: "Veg Sandwich", price: 30 },
-      { name: "Veg Cheese Sandwich", price: 40 },
-      { name: "French Fries", price: 20 },
-      { name: "Masala French Fries", price: 25 },
-      { name: "Peri Peri French Fries", price: 30 },
-      { name: "Veg Burger", price: 50 },
-      // { name: "Veg Cheese Burger", price: 60 },
-      // { name: "Veg Biryani", price: 80 }
+      { name: "Veg Roll", price: 60 },
+      { name: "Egg Roll", price: 70 },
+      { name: "Double Egg Roll", price: 80 },
+      { name: "Chicken Roll", price: 80 },
+      { name: "Lemon Honey Special Roll", price: 100 },
+      { name: "Veg Spring Roll", price: 60 },
+      { name: "Chicken Spring Roll", price: 80 },
     ]
   },
   {
-    category: "Juices",
+    category: "Momo",
     items: [
-      { name: "Lemon Juice", price: 20 },
-      // { name: "Lemon Soda", price: 30 },
+      { name: "Veg Momo", price: 50 },
+      { name: "Corn & Cheese Momo", price: 80 },
+      { name: "Chicken Momo", price: 60 },
+      { name: "Chicken Cheese Momo", price: 70 },
+      { name: "Veg Fried Momo", price: 60 },
+      { name: "Corn & Cheese Fried Momo", price: 90 },
+      { name: "Chicken Fried Momo", price: 70 },
+      { name: "Chicken Cheese Fried Momo", price: 80},
+      { name: "Veg Momo Plater 10pcs (fried+steam+ makhani+tandoori+malai cheese)", price: 120},
+      { name: "Chicken Momo Plater 10pcs (fried+steam+ makhani+tandoori+malai cheese)", price: 140}
+    ]
+  },
+  {
+    category: "Thukpa",
+    items: [
+      { name: "Veg Thukpa", price: 80 },
+      { name: "Chicken Thukpa", price: 100 }
+    ]
+  },
+  {
+    category: "Sandwich, Burger & Hot dogs",
+    items: [
+      { name: "Veg Sandwich", price: 40 },
+      { name: "Veg Cheese Sandwich", price: 50 },
+      { name: "Veg Burger", price: 60 },
+      { name: "Chicken Burger", price: 70 },
+      { name: "Veg Hot dog", price: 70 },
+      { name: "Non Veg Hot dog", price: 80 },
+    ]
+  },
+  {
+    category: "Veg Snacks",
+    items: [
+      { name: "French Fries", price: 40 },
+      { name: "Peri Peri French Fries", price: 50 },
+      { name: "Crispy Chilli Babycorn", price: 80 },
+      { name: "Honey Chilli Potato", price: 80 },
+    ]
+  },
+  {
+    category: "Dosa",
+    items: [
+      { name: "Plain Dosa", price: 40 },
+      { name: "Masala Dosas", price: 50 },
+      { name: "Egg Dosa", price: 60 },
+      { name: "Cheese Masala Dosa", price: 60 },
+      { name: "Cheese Egg Dosa", price: 70 },
+    ]
+  },
+  {
+    category: "Dosa",
+    items: [
+      { name: "Plain Dosa", price: 40 },
+      { name: "Masala Dosas", price: 50 },
+      { name: "Egg Dosa", price: 60 },
+      { name: "Cheese Masala Dosa", price: 60 },
+      { name: "Cheese Egg Dosa", price: 70 },
+    ]
+  },
+  {
+    category: "Chicken snacks",
+    items: [
+      { name: "Fried Chicken Kabab Mini(5pcs)", price: 50 },
       {
-        name: "Mix Fruit Juice (Banana, Apple, Milk, Honey, Grapes, Almonds, Dates, Watermelon)",
-        price: 50
+        name: "Fried Chicken Kabab Large(10 pcs)",
+        price: 90
       },
-      { name: "Mosambi Juice", price: 40 },
-      { name: "Grape Juice", price: 40 },
-      { name: "Apple Juice", price: 50 },
-      { name: "Pineapple Juice", price: 40 },
-      { name: "Watermelon Juice", price: 50 }
+      { name: "Crispy Chicken dry", price: 90 },
+      { name: "Crispy Chicken Thick Gravy", price: 100 },
+      { name: "Chicken Pakora Mini(5pcs)", price: 50 },
+      { name: "Chicken Pakora Large(10 pcs)", price: 90 },
+      { name: "Lemon Honey Chicken", price: 100 },
+      { name: "chicken Wings", price: 120 },
+      { name: "Chilli Chicken", price: 80 },
+      { name: "Chicken Nuggets", price: 100 },
+      { name: "Drums of Heaven", price: 120 },
+    ]
+  },
+  {
+    category: "Noodles",
+    items: [
+      { name: "Veg Hakka Noodles", price: 60 },
+      { name: "Veg  Schezwan Hakka Noodles", price: 65 },
+      { name: "Egg Hakka Noodles", price: 75 },
+      { name: "Egg Schezwan Hakka Noodles", price: 80 },
+      { name: "Chicken Hakka Dosa", price: 90 },
+      { name: "Chicken Schezwan Hakka Dosa", price: 95 },
+    ]
+  },
+  {
+    category: "Rice",
+    items: [
+      { name: "Veg Fried Rice", price: 60 },
+      { name: "Veg Schezwan Fried Rice", price: 65 },
+      { name: "Egg Fried Rice", price: 75 },
+      { name: "Egg Schezwan Fried Rice", price: 80 },
+      { name: "Chicken Fried Rice", price: 90 },
+      { name: "Chicken Schezwan Fried Rice", price: 95 },
+      { name: "Veg Briyani", price: 100 },
+      { name: "Egg Biryani", price: 110 },
+      { name: "Chicken Biryani", price: 130 },
     ]
   },
   {
@@ -33,11 +122,10 @@ export const menuItems = [
     items: [
       { name: "Black Tea", price: 10 },
       { name: "Milk Tea", price: 10 },
-      { name: "Ginger Tea", price: 10 },
-      { name: "Cardamom/Elaichi Tea", price: 10 },
-      { name: "Lemon Honey Warm Water", price: 15 },
-      { name: "Cappuccino", price: 20 },
-      { name: "Black Coffee", price: 10 }
+      { name: "Milk Tea", price: 10 },
+      { name: "Cappuccino", price: 25 },
+      { name: "Black Coffee", price: 10 },
+      { name: "Lemon Honey Warm Water", price: 30 }
     ]
   },
   {
@@ -49,23 +137,23 @@ export const menuItems = [
       { name: "Dry Fruit Milkshake", price: 70 }
     ]
   },
-  {
-    category: "More……..",
-    items: [
-      // { name: "Cold Coffee", price: 50 },
-      { name: "Fruit Bowl (A bowl of sliced 5 different fruits)", price: 40 }
-    ]
-  },
-  {
-    category: "Coming Soon…………..",
-    items: [
-      { name: "Non Veg Biryani" },
-      { name: "Non Veg Burger" },
-      { name: "Non Veg Sandwich" },
-      { name: "Chicken Nuggets" },
-      { name: "Crispy Chicken" },
-      { name: "More Fruit Items" },
-      { name: "Momo and Many More……………………" }
-    ]
-  }
+  // {
+  //   category: "More……..",
+  //   items: [
+  //     // { name: "Cold Coffee", price: 50 },
+  //     { name: "Fruit Bowl (A bowl of sliced 5 different fruits)", price: 40 }
+  //   ]
+  // },
+  // {
+  //   category: "Coming Soon…………..",
+  //   items: [
+  //     { name: "Non Veg Biryani" },
+  //     { name: "Non Veg Burger" },
+  //     { name: "Non Veg Sandwich" },
+  //     { name: "Chicken Nuggets" },
+  //     { name: "Crispy Chicken" },
+  //     { name: "More Fruit Items" },
+  //     { name: "Momo and Many More……………………" }
+  //   ]
+  // }
 ];
